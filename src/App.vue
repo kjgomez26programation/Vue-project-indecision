121e2ec1cf3277a0e8f462ef803cb8cc0d228b6b
Jhojana Gomez <118693498+kjgomez26programation@users.noreply.github.com>
2023-11-30 11:57:43 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <IndecisionComponents/>
+</template>
+
+<script>
+import IndecisionComponents from './components/IndecisionComponents.vue'
+
+// import CounterComponent from './components/CounterComponent'
+
+   
+
+export default {
+  name: 'App',
+  components: {
+    IndecisionComponents,
+  // CounterComponent,
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
